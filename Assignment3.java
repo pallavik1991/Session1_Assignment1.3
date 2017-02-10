b10e8d2f3860486ba84c80838212eea12b9b9e06
pallavik1991 <pallu.katti@gmail.com>
2017-02-10 11:14:42 -0800

    Add files via upload

---
+++ Assignment3.java
@@ -0,0 +1,17 @@
+class Assignment3
+{
+public static void main(String[] ar)
+{
+	int a,b;
+	a=10;
+	b=20;
+
+	//swapping
+	a=a+b;
+	b=a-b;
+	a=a-b;
+
+	System.out.println("After swapping:");
+	System.out.println("a="+a+"\tb="+b);
+}
+}
